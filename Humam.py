a5f95db558a722c997f3fe28913085777a94b677
VlexUIA <xfalkon61@gmail.com>
2025-11-24 21:35:56 +0100

    Update Humam.py

--- Humam.py
+++ Humam.py
@@ -1,1 +1,3 @@
 
+Desverre, må vi informere at Humam har ikke gjort sin del av oppgaven, og egentlig ingen av delene som måtte gjøres innen denne oppgaven. 
+Imens resten av gruppen har jobbet med oppgaven, har Humam ikke vist noe interesse i å jobbe eller hjelpe. 
