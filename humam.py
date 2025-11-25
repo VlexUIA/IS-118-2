11a963ad33bdf12bf76d915117b52da71803e290
VlexUIA <xfalkon61@gmail.com>
2025-11-25 15:54:30 +0100

    Update humam.py

--- humam.py
+++ humam.py
@@ -1,7 +1,8 @@
 
 Desverre må vi informere at Humam har ikke bidratt i å gjøre oppgaven "Innleveringsoppgave 2 – IS-118" i det hele tatt, og har ikke vært en del av gruppa til å levere den. 
 Gjennom alle dagene vi har jobbet med innleveringen, har han ikke vist noe innsats til å bidra, hjelpe eller gjøre noen av delene + at "del 2" som handler om koding, begynte han å jobbe med den 25.11.2025, altså
-datoen når oppgaven skulle leveres inn, så han hadde ca. 3 timer på seg, og gjennom di 3 timene har han ikke vist noe innsats eller lyst til å faktisk bli ferdig med oppgaven. 
+datoen når oppgaven skulle leveres inn, så han hadde ca. 3 timer på seg, og gjennom di 3 timene har han ikke vist noe innsats eller lyst til å faktisk bli ferdig med oppgaven + Humam har ikke "joinet" Githuben engang, 
+som viser innsatsen han la inn.
 
 Gjennom alle dagene vi har jobbet med oppgaven, og alle møtene vi har hatt - har han ikke vært en del av di, og har ikke spurt om hjelp til å gjøre oppgaven, utenatt 25.11.2025, altså når han hadde 3 timer igjen på seg.
 
